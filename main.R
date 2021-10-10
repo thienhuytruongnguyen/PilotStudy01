ec1d78cd0f4c8e3f6f2812d08a0090f41e19e4fa
Thien <thienhuytruongnguyen@adelaide.edu.au>
2021-10-10 15:20:23 +1030

    update main script

--- main.R
+++ main.R
@@ -2,3 +2,7 @@
 RainDat <- read.csv("69049_SILO_Rain.csv")
 EvapDat <- read.csv("69049_SILO_Rain.csv")
 FlowDat <- read.csv("215004_HRS_Flow.csv")
+
+##Fit the WGEN model and Generate some rainfall replicates
+#>install.packages("C:/Research/Programming/PilotStudy01/Packages/TSformat",repos = NULL, type = "source")
+library(FormatTimeSeries)

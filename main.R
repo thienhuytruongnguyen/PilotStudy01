d05bd633ba121b0e46056cbbdc373437622814f8
Thien <thienhuytruongnguyen@adelaide.edu.au>
2021-10-12 14:20:28 +1030

    minor update

--- main.R
+++ main.R
@@ -1,24 +1,24 @@
 #Require local WGEN package
 install.packages("WGEN",repos = NULL,type = "source")
-library(WGEN)
 
 ##Get flow, rain and evap data
 RainDat <- read.csv("69041_SILO_Rain.csv")
 EvapDat <- read.csv("69041_SILO_Evap.csv")
 FlowDat <- read.csv("215004_HRS_Flow.csv")
+
 ####---Add 0.1 to FlowDat
-FlowDat_1 <- FlowDat[,2]+1
+#FlowDat_1 <- FlowDat[,2]+1
+
 ##Fit the WGEN model and Generate some rainfall replicates
-RainDatFormat <- format_TimeSeries(RainDat)
-SimRainList <- getSimRain(RainDatFormat, rep = 10, mod = "expo")
+RainDatFormat <- WGEN::format_TimeSeries(RainDat)
+SimRainList <- WGEN::getSimRain(RainDatFormat, rep = 10, mod = "gama")
 
 ##---------------------------------------------------------------------#
-#Added a new line
 
 library(airGR)
 DATA <- data.frame(matrix(NA,nrow = length(RainDat[,1]), ncol = 4))
 colnames(DATA) <- c("Date_Time","P","Q","E")
-DATA[,1] <- RainDat[,1]; DATA[,2] <- RainDat[,2]; DATA[,3] <- FlowDat_1; DATA[,4] <- EvapDat[,2]
+DATA[,1] <- RainDat[,1]; DATA[,2] <- RainDat[,2]; DATA[,3] <- FlowDat[,2]; DATA[,4] <- EvapDat[,2]
 
 DATA$Date_Time <- strptime(as.character(DATA$Date_Time), "%d/%m/%Y")
 DATA$Date_Time <- format(DATA$Date_Time,"%Y-%m-%d")
@@ -26,35 +26,36 @@ DATA$Date_Time <- format(DATA$Date_Time,"%Y-%m-%d")
 Dates <- as.POSIXlt(DATA$Date_Time,tz="",format="%Y-%m-%d")#Format date string
 
 #InputsModel object
-InputsModel <- CreateInputsModel(FUN_MOD = RunModel_GR4J, DatesR = Dates,
+InputsModel <- airGR::CreateInputsModel(FUN_MOD = airGR::RunModel_GR4J, DatesR = Dates,
                                  Precip = DATA$P, PotEvap = DATA$E)
 
 #RunOptions object
-##1.Index Run
+##1.Index Run and WarmUp period
 Ind_Run <- seq(which(format(Dates, format = "%Y-%m-%d") == "1960-01-01"),
                which(format(Dates, format = "%Y-%m-%d") == "2019-02-28"))
-
+Ind_WarmUp <- seq(which(format(Dates, format = "%Y-%m-%d") == "1950-01-01"),
+               which(format(Dates, format = "%Y-%m-%d") == "1959-12-31"))
 ##2.Run Option
-RunOptions <- CreateRunOptions(FUN_MOD = RunModel_GR4J,
+RunOptions <- airGR::CreateRunOptions(FUN_MOD = airGR::RunModel_GR4J,
                                InputsModel = InputsModel, IndPeriod_Run = Ind_Run,
-                               IniStates = NULL, IniResLevels = NULL, IndPeriod_WarmUp = NULL)
+                               IniStates = NULL, IniResLevels = NULL, IndPeriod_WarmUp = Ind_WarmUp)
 
 #InputsCrit object
-InputsCrit <- CreateInputsCrit(FUN_CRIT = ErrorCrit_NSE, InputsModel = InputsModel, 
+InputsCrit <- airGR::CreateInputsCrit(FUN_CRIT = airGR::ErrorCrit_NSE, InputsModel = InputsModel, 
                                RunOptions = RunOptions, VarObs = "Q", Obs = DATA$Q[Ind_Run])
 
 #CalibOptions object
-CalibOptions <- CreateCalibOptions(FUN_MOD = RunModel_GR4J, FUN_CALIB = Calibration_Michel)
+CalibOptions <- airGR::CreateCalibOptions(FUN_MOD = airGR::RunModel_GR4J, FUN_CALIB = airGR::Calibration_Michel)
 
 #CALIBRATION
-OutputsCalib <- Calibration_Michel(InputsModel = InputsModel, RunOptions = RunOptions,
+OutputsCalib <- airGR::Calibration_Michel(InputsModel = InputsModel, RunOptions = RunOptions,
                                    InputsCrit = InputsCrit, CalibOptions = CalibOptions,
-                                   FUN_MOD = RunModel_GR4J)
+                                   FUN_MOD = airGR::RunModel_GR4J)
 #Get GR4J parameter
 Param <- OutputsCalib$ParamFinalR
 
 #Run GR4J
-OutputsModel <- RunModel_GR4J(InputsModel = InputsModel, RunOptions = RunOptions, Param = Param)
+OutputsModel <- airGR::RunModel_GR4J(InputsModel = InputsModel, RunOptions = RunOptions, Param = Param)
 
 #Plot
 plot(OutputsModel, Qobs = DATA$Q[Ind_Run])
